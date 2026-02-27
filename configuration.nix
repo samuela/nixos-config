f059c8aa0c3187e875314aebe4cad7634ae9611d
Samuel Ainsworth <skainsworth@gmail.com>
2026-02-27 00:52:03 -0500

    add nixbuild.net remote builder

--- configuration.nix
+++ configuration.nix
@@ -213,6 +213,31 @@ in
 
   ### End battery, swap, and hibernation
 
+  # nixbuild.net remote builder
+  programs.ssh.extraConfig = ''
+    Host eu.nixbuild.net
+      PubkeyAcceptedKeyTypes ssh-ed25519
+      ServerAliveInterval 60
+      IPQoS throughput
+      IdentityFile /home/skainswo/.ssh/id_ed25519
+  '';
+  programs.ssh.knownHosts = {
+    nixbuild = {
+      hostNames = [ "eu.nixbuild.net" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
+    };
+  };
+  nix.distributedBuilds = true;
+  nix.buildMachines = [
+    {
+      hostName = "eu.nixbuild.net";
+      system = "x86_64-linux";
+      maxJobs = 100;
+      supportedFeatures = [ "benchmark" "big-parallel" ];
+    }
+  ];
+  nix.settings.builders-use-substitutes = true;
+
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
