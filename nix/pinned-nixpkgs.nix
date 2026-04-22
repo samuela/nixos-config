029746ee9e8b696a19e141a49cb121f194662525
Samuel Ainsworth <skainsworth@gmail.com>
2026-04-22 19:16:46 -0400

    nix: bump pinned nixpkgs to latest nixos-25.11

--- nix/pinned-nixpkgs.nix
+++ nix/pinned-nixpkgs.nix
@@ -4,11 +4,12 @@
 #   curl -s 'https://status.nixos.org/prometheus/api/v1/query?query=channel_revision' \
 #     | jq -r '.data.result[] | select(.metric.channel=="nixos-25.11" and .metric.current=="1") | .metric.revision'
 # Last updated: 2026-04-22.
+# When bumping this pin, update this date too.
 let
-  rev = "c7f47036d3df2add644c46d712d14262b7d86c0c";
+  rev = "10e7ad5bbcb421fe07e3a4ad53a634b0cd57ffac";
 in
 {
   inherit rev;
-  sha256 = "1aclyh8aysw0d8gb1k9hh7mcklkqfvvv4f86l9zcipi4r0s9fal3";
+  sha256 = "0x1ip9whw4djx0vlzyqagfifbq3v2m1s11yv4bn0rrj4369dspdy";
   url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
 }
